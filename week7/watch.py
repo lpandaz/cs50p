5d575184c42ab5f082a218d1ca1d38391ce35b3c
lpandaz <norkk124@gmail.com>
2026-02-16 23:34:29 +0900

    Add files via upload

--- week7/watch.py
+++ week7/watch.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def main():
@@ -7,12 +6,14 @@ def main():
 
 
 def parse(s):
-    
-    first_search = re.search(r"^<iframe (\w)+> </iframe>$",s)
-    if first_search:
-        return True
+
+    first_search = re.search(r'src="(.+?)"', s)
+    if first_search and 'youtube.com' in first_search.group(1) and 'iframe' in s:
+        second_search = re.search(r'embed/(\w)+', first_search.group(1))
+        embed, link = second_search.group(0).split('/')
+        return f"https://youtu.be/{link}"
     else:
-        return False
+        return None
 
 ...
 
